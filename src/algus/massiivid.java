b48c0e6765636e044914e4c45ac1f682ba9242e0
Kertu Mikk <31847340+KertuMikk@users.noreply.github.com>
2018-02-01 10:43:34 +0200

    toatemperatuur lisatud

--- src/algus/massiivid.java
+++ src/algus/massiivid.java
@@ -1,11 +1,24 @@
 
 package algus;
 
+import java.util.Arrays;
 /**
  *
  * @author ketuo
  */
 public class massiivid {
+    public static int summa(int[] arvud){
+     int summa=0;
+
+      for(int i=0; i<arvud.length; i++){
+
+         summa+=arvud[i];
+
+      } 
+      return summa;
+        
+        
+    }
     public static void main(String[] arg){
      int[] pikkused={176, 163, 158, 171, 169}; 
      System.out.println("Kokku: "+pikkused.length);
@@ -17,6 +30,23 @@ public class massiivid {
         sum = sum + pikkused[i];
      }
      System.out.println("summa on: "+sum);
+     int summa=0;
+
+      for(int i=0; i<pikkused.length; i++){
+
+         summa+=pikkused[i];
+
+      }
+
+      System.out.println(summa);
+
+      summa=0;
+
+      for(int pikkus: pikkused){summa+=pikkus;}
+
+      System.out.println(summa);
+
+      System.out.println(Arrays.stream(pikkused).sum());
      }
     
     }
