37ab7d2eccc3464822c5b8b15ed1951f863b557c
Kertu Mikk <31847340+KertuMikk@users.noreply.github.com>
2018-02-01 09:44:59 +0200

    esimene katsetus

---
+++ src/algus/Algus.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algus;
+
+/**
+ *
+ * @author ketuo
+ */
+public class Algus {
+    public static void ilmakommentaar(int temperatuur){
+        if(temperatuur>0){System.outPrintIn("sulab");
+        }
+        else{System.out.printIn("jäätab");
+        }
+    }
+    public static void toatemperatuur(int tuba){
+        if(tuba < 35){System.out.printIn("pane kütet juurde")}
+        else {System.out.printIn("piisavalt soe")
+        }
+        
+        
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printIn("Hello,Web");
+        ilmakommentaar(3);
+    }
+    
+}
