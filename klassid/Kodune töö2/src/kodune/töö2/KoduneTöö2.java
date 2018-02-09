47b628c1711765f6f8debd5e8a7cdb8a6a4856e1
Kertu Mikk <kertumik@tlu.ee>
2018-02-09 15:58:26 +0200

    kehamassi indeksid

--- klassid/Kodune töö2/src/kodune/töö2/KoduneTöö2.java
+++ klassid/Kodune töö2/src/kodune/töö2/KoduneTöö2.java
@@ -1,21 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kodune.töö2;
 
-/**
- *
- * @author ketuo
- */
-public class KoduneTöö2 {11
+import static java.lang.Math.round;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+
+public class KoduneTöö2 {
+
+    
+   public static void main(String[] arg) {
+   indeks p1=new indeks(65, 168);
+   indeks p2= new indeks(70, 150);
+   System.out.println(p1);
+   System.out.println(p2); 
+   System.out.println("esimese persooni kehamassiindeks on: "+round(p1.KMI())+" ja "+p1.seisund());
+   System.out.println("teise persooni kehamassiindeks on: "+round(p2.KMI())+" ja "+p2.seisund());
+   System.out.println(p1.Suurem(p2));
+  
     }
     
+   
 }
+/*Persoon (65.0 kg, 168.0cm)
+Persoon (70.0 kg, 150.0cm)
+esimese persooni kehamassiindeks on: 23 ja Persoon on ideaalkaalus
+teise persooni kehamassiindeks on: 31 ja Persoon on rasvunud
+Teise persooni KMI on suurem*/
---
+++ klassid/Kodune töö2/src/kodune/töö2/indeks.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kodune.töö2;
+
+public class indeks {
+    protected double x;
+    protected double y;
+ 
+    
+    public indeks(double x, double y){
+      this.x=x;
+      this.y=y;
+      
+}
+    public double getX(){return x;}
+    public double getY(){return y;}
+    public double KMI(){
+    double kmi=x/((y*0.01)*(y*0.01));    
+    return kmi;    
+    }
+    
+    
+    
+    public String seisund(){
+        
+        if (KMI() >= 30) {
+            return "Persoon on rasvunud";
+        } else if ((30>KMI())&&(KMI()>25)) {
+            return "Persoon on ülekaalus";
+        } else if ((25>KMI())&&(KMI()>19)) {
+            return "Persoon on ideaalkaalus";
+        } else {
+            return "Persoon on alakaalus";
+        } 
+        
+        
+        
+      
+                
+            }         
+    
+    public String Suurem(indeks teine){
+    if((x/((y*0.01)*(y*0.01)))>(getX()/((getY()*0.01)*(getY()*0.01)))){
+        return "Esimese persooni KMI on suurem";
+    }else{
+      return "Teise persooni KMI on suurem";  
+    }
+        
+        
+    }
+    
+    
+    @Override
+    public String toString(){
+     return "Persoon ("+x+" kg, "+y+"cm)"; 
+    }
+}
