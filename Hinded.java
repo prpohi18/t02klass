776a2ba62de597bc74462acc42c50136d8c5fd6b
Kert Tamm <31847373+K3RTTAMM@users.noreply.github.com>
2018-02-11 21:25:24 +0200

    added functionality for 5 different subjects and made other major changes

--- Hinded.java
+++ Hinded.java
@@ -1,33 +1,38 @@
 public class Hinded{
-	protected double kood;
+	protected String opilasenimi;
+	protected String kood;
+	protected String aine1nimi;
 	protected double aine1eap;
 	protected double aine1hinne;
+	protected String aine2nimi;
 	protected double aine2eap;
 	protected double aine2hinne;
+	protected String aine3nimi;
 	protected double aine3eap;
 	protected double aine3hinne;
+	protected String aine4nimi;
+	protected double aine4eap;
+	protected double aine4hinne;
+	protected String aine5nimi;
+	protected double aine5eap;
+	protected double aine5hinne;
 	
-	public double hinnesum(int aine1hinne, int aine2hinne, int aine3hinne){
-		double HIN=aine1hinne+aine2hinne+aine3hinne;
+	public double hinnesum(int aine1hinne, int aine2hinne, int aine3hinne, int aine4hinne, int aine5hinne){
+		double HIN=aine1hinne+aine2hinne+aine3hinne+aine4hinne+aine5hinne;
 		return HIN;	
 	}
 		
-	public double eapsum(int aine1eap, int aine2eap, int aine3eap){
-		double EAP=aine1eap+aine2eap+aine3eap;
-		return EAP;	
-	
-		
+	public double eapsum(int aine1eap, int aine2eap, int aine3eap, int aine4eap, int aine5eap){
+		double EAP=aine1eap+aine2eap+aine3eap+aine4eap+aine5eap;
+		return EAP;				
 	}
 	
-	public double kkh(int aine1eap, int aine1hinne, int aine2eap, int aine2hinne, int aine3eap, int aine3hinne){
-		
-		double KKH=((aine1eap*aine1hinne)+(aine2eap*aine2hinne)+(aine3eap*aine3hinne)/(aine1eap+aine2eap+aine3eap));
+	public double kkh(int aine1eap, int aine1hinne, int aine2eap, int aine2hinne, int aine3eap, int aine3hinne, int aine4eap, int aine4hinne, int aine5eap, int aine5hinne){		
+		double A=(aine1eap*aine1hinne)+(aine2eap*aine2hinne)+(aine3eap*aine3hinne)+(aine4eap*aine4hinne)+(aine5eap*aine5hinne);
+		double KKH = A/(aine1eap+aine2eap+aine3eap+aine4eap+aine5eap);
 		return KKH;			
 		
-	}
-		
-	
-	
+	}	
 }
 
 
--- Opilased.java
+++ Opilased.java
@@ -5,9 +5,14 @@ public class Opilased{
 	public static void main(String[] args)throws IOException{
 		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
 
+		System.out.println("Sisesta õpilase nimi:");
+		String opilasenimi=buffer.readLine();
+		
 		System.out.println("Sisesta õpilase kood:");
-		String Kood=buffer.readLine();
-		int kood=Integer.parseInt(Kood);		
+		String kood=buffer.readLine();
+		
+		System.out.println("Sisesta 1. aine nimetus:");
+		String aine1nimi=buffer.readLine();
 			
 		System.out.println("Sisesta aine1 EAP:");
 		String Aine1EAP=buffer.readLine();
@@ -17,6 +22,9 @@ public class Opilased{
 		String Aine1HINNE=buffer.readLine();
 		int aine1hinne=Integer.parseInt(Aine1HINNE);
 		
+		System.out.println("Sisesta 2. aine nimetus:");
+		String aine2nimi=buffer.readLine();
+		
 		System.out.println("Sisesta aine2 EAP:");
 		String Aine2EAP=buffer.readLine();
 		int aine2eap=Integer.parseInt(Aine2EAP);
@@ -24,6 +32,9 @@ public class Opilased{
 		System.out.println("Sisesta aine2 hinne:");
 		String Aine2HINNE=buffer.readLine();
 		int aine2hinne=Integer.parseInt(Aine2HINNE);
+		
+		System.out.println("Sisesta 3. aine nimetus:");
+		String aine3nimi=buffer.readLine();
 		
 		System.out.println("Sisesta aine3 EAP:");
 		String Aine3EAP=buffer.readLine();
@@ -33,20 +44,59 @@ public class Opilased{
 		String Aine3HINNE=buffer.readLine();
 		int aine3hinne=Integer.parseInt(Aine3HINNE);
 		
+		System.out.println("Sisesta 4. aine nimetus:");
+		String aine4nimi=buffer.readLine();
+		
+		System.out.println("Sisesta aine4 EAP:");
+		String Aine4EAP=buffer.readLine();
+		int aine4eap=Integer.parseInt(Aine4EAP);
+
+		System.out.println("Sisesta aine4 hinne:");
+		String Aine4HINNE=buffer.readLine();
+		int aine4hinne=Integer.parseInt(Aine4HINNE);
+		
+		System.out.println("Sisesta 5. aine nimetus:");
+		String aine5nimi=buffer.readLine();
+		
+		System.out.println("Sisesta aine5 EAP:");
+		String Aine5EAP=buffer.readLine();
+		int aine5eap=Integer.parseInt(Aine5EAP);
+
+		System.out.println("Sisesta aine5 hinne:");
+		String Aine5HINNE=buffer.readLine();
+		int aine5hinne=Integer.parseInt(Aine5HINNE);
+		
 		Hinded opilane  = new Hinded();
 		
+		opilane.opilasenimi = opilasenimi;
 		opilane.kood = kood;
+		opilane.aine1nimi = aine1nimi;
 		opilane.aine1eap = aine1eap;
 		opilane.aine1hinne = aine1hinne;
+		opilane.aine2nimi = aine2nimi;
 		opilane.aine2eap = aine2eap;
 		opilane.aine2hinne = aine2hinne;
+		opilane.aine3nimi = aine3nimi;
 		opilane.aine3eap = aine3eap;
 		opilane.aine3hinne = aine3hinne;	
+		opilane.aine4nimi = aine4nimi;
+		opilane.aine4eap = aine4eap;
+		opilane.aine4hinne = aine4hinne;
+		opilane.aine5nimi = aine5nimi;
+		opilane.aine5eap = aine5eap;
+		opilane.aine5hinne = aine5hinne;
 	
-		double hinnesum = opilane.hinnesum(aine1hinne, aine2hinne, aine3hinne);	
-		double eapsum = opilane.eapsum(aine1eap, aine2eap, aine3eap);	
-		double kkh = opilane.kkh(aine1eap, aine1hinne, aine2eap, aine2hinne, aine3eap, aine3hinne);	
-		System.out.printf("Õpilase kood: %.0f \nÕppekava täidetud mahus: %.0fEAP \nHinnete summa: %.2f \nKKH: %.2f", opilane.kood, eapsum, hinnesum, kkh);	
+		double hinnesum = opilane.hinnesum(aine1hinne, aine2hinne, aine3hinne, aine4hinne, aine5hinne);	
+		double eapsum = opilane.eapsum(aine1eap, aine2eap, aine3eap, aine4eap, aine5eap);	
+		double kkh = opilane.kkh(aine1eap, aine1hinne, aine2eap, aine2hinne, aine3eap, aine3hinne, aine4eap, aine4hinne, aine5eap, aine5hinne);	
+		System.out.printf("\nÕpilase nimi: %s \nÕpilase kood: %s \nÕppekava maht: %.0fEAP \nHinnete summa: %.2f \nKKH: %.2f", opilane.opilasenimi, opilane.kood, eapsum, hinnesum, kkh);
+		System.out.printf("\nAine: %s (%.0fEAP HINNE: %.0f)\nAine: %s (%.0fEAP HINNE: %.0f)\nAine: %s (%.0fEAP HINNE: %.0f)\nAine: %s (%.0fEAP HINNE: %.0f)\nAine: %s (%.0fEAP HINNE: %.0f)", 
+				opilane.aine1nimi, opilane.aine1eap, opilane.aine1hinne, 
+				opilane.aine2nimi, opilane.aine2eap, opilane.aine2hinne,
+				opilane.aine3nimi, opilane.aine3eap, opilane.aine3hinne,
+				opilane.aine4nimi, opilane.aine4eap, opilane.aine4hinne,
+				opilane.aine5nimi, opilane.aine5eap, opilane.aine5hinne);		
+
 	}
 }
 
