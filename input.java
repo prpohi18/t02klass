71f6a0d95100a68c990ed33ce52b47929710eb1b
Martin Sõmer <31840332+martinsomer@users.noreply.github.com>
2018-02-12 08:27:25 +0200

    Added examples

--- input.java
+++ input.java
@@ -14,3 +14,23 @@ public class input {
         }
     }
 }
+
+/*
+
+>java input 1999 2000 2001 2002 2003 2004 2005
+1999 ei ole liigaasta.
+2000 on liigaasta.
+2001 ei ole liigaasta.
+2002 ei ole liigaasta.
+2003 ei ole liigaasta.
+2004 on liigaasta.
+2005 ei ole liigaasta.
+
+>java input
+Argumendid puuduvad.
+
+>java input tere
+Programmis tekkis järgmine viga:
+java.lang.NumberFormatException: For input string: "tere"
+
+*/
