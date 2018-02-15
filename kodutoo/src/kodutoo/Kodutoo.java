c610dfc4bbc825db192d094665766e386c9b5177
zeus <zeus@A406-17.INFORMAATIKA>
2018-02-15 09:37:07 +0200

    Teet Triisa Kodune 2

---
+++ kodutoo/src/kodutoo/Kodutoo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kodutoo;
+
+/**
+ *
+ * @author zeus
+ */
+public class Kodutoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        warrior p1 = new warrior(20, 5, 1);
+        warrior p2 = new warrior(20, 4, 2);
+        System.out.println(p1);
+        System.out.println(p2);
+        p1.fight(p2);
+    }
+    
+}
---
+++ kodutoo/src/kodutoo/warrior.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kodutoo;
+
+/**
+ *
+ * @author zeus
+ */
+public class warrior {
+    protected double health;
+    protected double attack;
+    protected double defense;
+    
+    public warrior(double health, double attack, double defense) {
+        this.health=health;
+        this.attack=attack;
+        this.defense=defense;
+    }
+    
+    public double getHealth() {
+        return health;
+    }
+    
+    public double getAttack() {
+        return attack;
+    }
+    
+    public double getDefense() {
+        return defense;
+    }
+    
+    public boolean fight(warrior teine) {
+        double w1Hp = getHealth();
+        double w1Att = getAttack();
+        double w1Def = getDefense();
+        double w2Hp = teine.getHealth();
+        double w2Att = teine.getAttack();
+        double w2Def = teine.getDefense();
+        
+        while(true) {
+            w2Hp = w2Hp-(w1Att-w2Def);
+            System.out.println("Sõdalane üks lõi teist sõdalast "+(w1Att-w2Def)+" võrra, sõdalasel elusid jäänud "+w2Hp+"/"+teine.getHealth());
+            if(w2Hp <= 0) {
+                System.out.println("Sõdalane üks võitis");
+                return true;
+            }
+            w1Hp = w1Hp-(w2Att-w1Def);
+            System.out.println("Sõdalane kaks lõi esimest sõdalast "+(w2Att-w1Def)+" võrra, sõdalasel elusid jäänud "+w1Hp+"/"+getHealth());
+            if(w1Hp <= 0) {
+                System.out.println("Sõdalane kaks võitis");
+                return false;
+            }
+        }
+
+    }
+    
+    @Override
+    public String toString(){
+        return "Sõdalane eludega: "+health+", ründega: "+attack+", kaitsega: "+defense;
+    }
+}
