f9e7088a0550f03e46e2a05179f24dcb73749e65
Kristo Roots <rootkris@users.noreply.github.com>
2018-02-27 12:59:58 +0200

    Kodunetöö nr2

---
+++ Proov1.java
@@ -0,0 +1,20 @@
+public class Proov1{
+	public static void main(String[] arg){
+		Numbrid tundmatu1=new Numbrid("37253065733");
+		Numbrid tundmatu2=new Numbrid("4657878827");
+		Numbrid tundmatu3=new Numbrid("4755667788");
+		Numbrid tundmatu4=new Numbrid("37158589098");
+		Numbrid tundmatu5=new Numbrid("37056432312");
+		Numbrid tundmatu6=new Numbrid("35854432343");
+		Numbrid tundmatu7=new Numbrid("37259008897");
+		Numbrid tundmatu8=new Numbrid("37251678727");
+		System.out.println(tundmatu1.Suunakood());
+		System.out.println(tundmatu2.Suunakood());
+		System.out.println(tundmatu3.Suunakood());
+		System.out.println(tundmatu4.Suunakood());
+		System.out.println(tundmatu5.Suunakood());
+		System.out.println(tundmatu6.Suunakood());
+		System.out.println(tundmatu7.Suunakood());
+		System.out.println(tundmatu8.Suunakood());
+	}
+}
