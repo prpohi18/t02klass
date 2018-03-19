5ad02c27e297e77081e4bf89fd5ad2570f745737
Kevin <Kevin@KOKSEN>
2018-03-19 19:31:10 +0200

    Kodutoo2

---
+++ Ristkylik/src/Ristkylik/Rist.java
@@ -0,0 +1,11 @@
+package Ristkylik;
+public class Rist {
+    public static void main (String[] args) {
+        Ristkylik laud1=new Ristkylik(2, 1);
+	Ristkylik laud2=new Ristkylik(1.5, 1);
+	Ristkylik laud3=new Ristkylik(2, 1.5);
+	System.out.println(laud1);
+	System.out.println(laud2);
+        System.out.println(laud3);
+    }
+}
