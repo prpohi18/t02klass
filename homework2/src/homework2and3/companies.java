2828c34221c2ac3e2f410dd5e31555ca61278d9a
Kaur K <kaurk@users.noreply.github.com>
2018-03-21 15:09:47 +0200

    Homework 2 + 3. Program for IT companies. Many classes and different objects, which communicate with each other and do calculations. All done!

--- homework2/src/homework2and3/companies.java
+++ homework2/src/homework2and3/companies.java
@@ -1,13 +1,13 @@
-package homework2;
+package homework2and3;
 import java.util.Calendar;
 
-public class companiesInfo {
+public class companies {
     protected String company;
     protected int revenue;
     protected int foundedAt;
     protected int employees;
     
-    public companiesInfo(String company, int revenue, int foundedAt, int employees){
+    public companies(String company, int revenue, int foundedAt, int employees){
         this.company = company;
         this.revenue=revenue;
         this.foundedAt=foundedAt;
