f8631166611c11db0be3b7fa1278256b9784cb99
Egert Klaamas <31847333+egert112@users.noreply.github.com>
2018-04-02 16:12:04 +0300

    Nelinurk, 2. kodutöö

---
+++ Kodutöö2/src/kodutöö2/Kodutöö2.java
@@ -0,0 +1,18 @@
+package kodutöö2;
+
+
+public class Kodutöö2 {
+
+   
+    public static void main(String[] args) {
+        Hommik nr1=new Hommik(2);
+        Hommik nr2=new Hommik(6);
+        Hommik nr3=new Hommik(22);
+        
+        System.out.println(nr1.toString());
+        System.out.println(nr2.toString());
+        System.out.println(nr3.toString());
+        
+    }
+    
+}
