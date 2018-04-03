136740647165d38a72e54aaf1a0422ced5a961a9
Konstnatin Blinkov <31948927+kostjaki@users.noreply.github.com>
2018-04-03 10:28:03 +0300

    Teine kodutöö

---
+++ Name.java
@@ -0,0 +1,11 @@
+public class Name {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.name = "Max";
+        Dog dog2 = new Dog();
+        dog2.name = "Bella";
+        Dog dog3 = new Dog();
+        dog3.name = "Jack";
+		System.out.println("Koerade nimed on " + dog1.name + ", " + dog2.name + " ja " + dog3.name);
+    }
+}
