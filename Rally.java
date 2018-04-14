fbb1bb7f49e4a2ced48b638fc8e4a9edef99178f
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-04-14 10:02:30 +0300

    Kodune 2
    Tehtud 2 kodune. Punktide arvutamine sõltuvalt Rally auto kohalt

---
+++ Rally.java
@@ -0,0 +1,10 @@
+public class Rally {
+  public static void main(String[] args) {
+        andmed auto1 = new andmed("1");
+        andmed auto2 = new andmed("5");
+        
+        System.out.println("Tulemuseks on "+auto1.arvestus());
+        System.out.println("Tulemuseks on "+auto2.arvestus());
+        
+    }
+}
